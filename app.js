1d5d04394c502001ede4dc52aff2efa0f8092430
william <william@dxt.cn>
2016-02-01 20:27:00 +0800

    lala

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-    var http = require('http');  
-      
-    var qs = require('querystring');  
-      
-    var data = {  
-       page:1,
-	   q:"周杰伦",
-       size:10
-     };//这是需要提交的数据  
-      
-      
-    var content = qs.stringify(data);  
-      
-    var options = {  
-        hostname: 'search.dongting.com',    
-        path: 'http://search.dongting.com/song/search/old?' + content,  
-        method: 'GET'  
-    };  
+var express = require('express');
+var search = require('./lib/controllers/search')
 
-    var req = http.request(options, function (res) {  
-       // console.log('STATUS: ' + res.statusCode);  
-       // console.log('HEADERS: ' + JSON.stringify(res.headers));  
-        res.setEncoding('utf8');  
-        res.on('data', function (chunk) {  
-            console.log('BODY: ' + chunk);  
-        });  
-    });  
-      
-    req.on('error', function (e) {  
-        console.log('problem with request: ' + e.message);  
-    });  
-      
-    req.end();  
+var app = express();
+app.get('/search', function (req, res) {
+    console.log(req.query);
+    search.createRequest(req.query, function (data) {
+        res.write(data);
+    });
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
