205e067de491ee231a9bb145bc330a310d550f23
william <william@dxt.cn>
2016-02-03 19:10:41 +0800

    完善

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ var express = require('express');
 var search = require('./lib/controllers/search')
 
 var app = express();
+app.all('*',function (req, res, next) {
+  res.header('Content-Type','application/json;charset=UTF-8');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); //让options请求快速返回
+  }
+  else {
+    next();
+  }
+});
 app.get('/search', function (req, res) {
     console.log(req.query);
-
     search.createRequest(req.query, function (sres) {
             sres.pipe(res);
             sres.on('end', function () {
