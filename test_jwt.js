22aa276b29be7d1b14b57bafad00cbed0115168d
biyao <biyao>
2016-03-18 18:47:51 +0800

    'jwt'

--- test_jwt.js
+++ test_jwt.js
@@ -1,10 +1,11 @@
 var jwt = require('jwt-simple');
+
 var payload = { foo: 'bar' };
 var secret = 'xxx';
 
 // encode
 var token = jwt.encode(payload, secret);
-
+console.log(token);
 // decode
 var decoded = jwt.decode(token, secret);
 console.log(decoded); //=> { foo: 'bar' }
